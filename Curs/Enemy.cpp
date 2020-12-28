5a5c81f5018fcf09707f5af3d959ab2dd3c0cca0
Anton Zudov <zudovanton1234@mail.ru>
2020-12-28 14:02:33 +0300

    Добавлен метод писка препятствия для класса врага

--- Curs/Enemy.cpp
+++ Curs/Enemy.cpp
@@ -10,3 +10,34 @@ Enemy::Enemy(Image &image, float X, float Y, int W, int H) :Entity(image, X, Y, 
 	speed = 0.1; //скорость
 	dx = speed;
 }
+
+void Enemy::checkCollisionWithMap(float Dx, float Dy)//проверка на препятствие
+{
+	for (int i = y / 32; i < (y + h) / 32; i++)//цикл проверки следующих элементов 
+	for (int j = x / 32; j<(x + w) / 32; j++)
+	{
+		if (mp.TileMap[i][j] == '0')//если впереди препятствие
+		{//в соответствие с выбранным направлением перемещает на предыдущий элемент
+			if (Dy > 0) { 
+				y = i * 32 - h;  dy = -0.1;
+				direction = rand() % (3); //случайное направление    
+			}//по Y    
+			if (Dy < 0) {
+				y = i * 32 + 32; dy = 0.1;
+				direction = rand() % (3); 
+			}   
+			if (Dx > 0) {
+				x = j * 32 - w; dx = -0.1;
+				direction = rand() % (3);     
+			} 
+			if (Dx < 0) {
+				x = j * 32 + 32; dx = 0.1;
+				direction = rand() % (3);
+			}
+
+		}
+	}
+}
+
+
+
