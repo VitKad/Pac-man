bef5b74499dd65317ae68f0748d271ef1b262e86
Anton Zudov <zudovanton1234@mail.ru>
2020-12-28 12:49:41 +0300

    Добавлен заголовочны файл врага

---
+++ Curs/Enemy.h
@@ -0,0 +1,14 @@
+#ifndef __ENEMY_H__
+#define __ENEMY_H__
+#include "stdafx.h"
+#include "Entity.h"
+class Enemy :public Entity{  //дочерний класс врага, призрака от класса сущности
+public:
+	int direction;//Направление врага 
+	Enemy(Image &image, float X, float Y, int W, int H);
+	void checkCollisionWithMap(float Dx, float Dy); //метод проверки на столкновение
+	void update(float time);//метод обновления поведения врага
+
+};
+
+#endif;
